7db2a09fac35cf1280e95c140dae35b615a185aa
Alexey <medvedev.lasta@yandex.ru>
2020-03-18 19:34:22 +0700

    initial

---
+++ app/src/main/java/com/technopolis/MainActivity.java
@@ -0,0 +1,14 @@
+package com.technopolis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
